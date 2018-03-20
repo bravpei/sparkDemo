5cc1d4177c08a7bbd954fd956a51b11db5a57e6d
liupei-gs <403664797@qq.com>
2018-03-20 11:18:10 +0800

    修改文件地址

--- src/main/java/moon/star/core/WordCount.scala
+++ src/main/java/moon/star/core/WordCount.scala
@@ -5,6 +5,6 @@ import org.apache.spark.{SparkConf, SparkContext}
 object WordCount extends App{
   val conf=new SparkConf().setAppName("wordCount").setMaster("local[*]")
   val sc=new SparkContext(conf)
-  val file=sc.textFile("/home/liupei/test/fiction.txt")
+  val file=sc.textFile("D:/test/fiction.txt")
   file.flatMap(_.split(" ")).map((_,1)).reduceByKey((_ + _)).filter(_._2>10).foreach(println)
 }
